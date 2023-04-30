078a02a43af49e60cfa57d72e75ee9e23b981a5b
badalya1 <badalyandavo@gmail.com>
2023-04-30 18:09:21 +0000

    Update from https://github.com/badalya1/portfolio-nuxt/commit/6ff4e95e84ee3cc8ea5e350b3037a8abebc1679e

--- public/_nuxt/darkMode.e7e10b56.js
+++
@@ -1,1 +0,0 @@
-import{M as o}from"./entry.48a430b6.js";const d=()=>{let e=o("darkMode",()=>!0);return typeof window>"u"||(console.log("Calculating dark mode"),e.value=window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches,window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",a=>{e.value=a.matches})),e};export{d as u};
